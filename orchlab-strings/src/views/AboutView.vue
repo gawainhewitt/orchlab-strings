71412b66a92981cbfa2e4dff76fa7cc2b618afda
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-05-22 16:29:55 +0100

    Create routes for Home, About and Instructions

---
+++ orchlab-strings/src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Has details about the project, what the aim is, who the target market/audience is</p>
+  </div>
+</template>
