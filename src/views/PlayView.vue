9a1b81bdfe59ccd099e5b28f9bdba8487e515a83
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2023-05-23 12:36:12 +0100

    button triggers method

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -2,5 +2,18 @@
   <div class="play">
     <h1>This is the play page</h1>
     <p>For playing the instrument</p>
+    <button @click="testing('bananaman')">Play</button>
   </div>
 </template>
+
+<script>
+
+export default {
+  methods: {
+    testing(message) {
+      console.log(message);
+    }
+  }
+}
+</script>
+
