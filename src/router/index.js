ef40e9215d6bf986b513ddb51b45fb282256ad42
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-05-23 08:43:35 +0100

    Change format of AboutView

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import InstructionsView from '../views/InstructionsView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const routes = [
   {
@@ -11,15 +12,12 @@ const routes = [
   {
     path: '/instructions',
     name: 'instructions',
-    component: InstructionsView,
+    component: InstructionsView
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
   }
 ]
 
