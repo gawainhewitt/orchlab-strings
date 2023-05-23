8461ca61442539cdbd5c4f5b7bcd0fbc0acf335e
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-05-23 08:09:40 +0100

    Change capitalisation on instructions

--- orchlab-strings/src/App.vue
+++ orchlab-strings/src/App.vue
@@ -2,7 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/instructions">instructions</router-link>
+    <router-link to="/instructions">Instructions</router-link>
   </nav>
   <router-view/>
 </template>
