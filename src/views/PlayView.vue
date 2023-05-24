9e8f3d79a598720e6e1360abe77bdd60c79d02fe
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-05-24 16:10:53 +0100

    Implement colourblind spectrum colours

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -20,6 +20,15 @@
 <script>
   import AppButton from "../components/AppButton.vue"
 
+  // const black = "rgb(0, 0, 0)"
+  const orange = "rgb(230, 159, 0)"
+  // const skyBlue = "rgb(86, 180, 233)"
+  const blueishGreen = "rgb(0, 158, 115)"
+  // const yellow = "rgb(240, 228, 66)"
+  // const blue = "rgb(0, 114, 178)"
+  const vermilion = "rgb(213, 94, 0)"
+  const reddishPurple = "rgb(204, 121, 167)"
+
     export default {
       name: "App",
       components: {
@@ -27,11 +36,11 @@
        },
       data() {
         return {
-          strings: [{name: "note1", note: "a", color: "red"}, 
-                    {name: "note2", note: "b", color: "blue"},
-                    {name: "note3", note: "c", color: "green"},
-                    {name: "note4", note: "d", color: "purple"}
-                  ],
+          strings: [{name: "note1", note: "a", color: orange}, 
+                    {name: "note2", note: "b", color: blueishGreen},
+                    {name: "note3", note: "c", color: vermilion},
+                    {name: "note4", note: "d", color: reddishPurple}
+                  ]
         }
       },
       methods: {
