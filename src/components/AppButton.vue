f7be27d5636289db9bdf6e7ca32f0493eb5785bb
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-05-24 08:15:29 +0100

    Buttons made on the playview page

---
+++ src/components/AppButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="dark baseButton">
+    {{ buttonText }}
+  </button>
+</template>
+
+
+<script>
+  export default {
+    props: {
+      buttonText: {
+        type: String,
+        default: () => "Label",
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+  /* .baseButton {
+    padding: 10px;
+    border: none;
+    radius: 
+  }
+
+  .light {
+    background: white;
+    color: black;
+  }
+
+  .dark {
+    background: black;
+    color: white;
+  } */
+</style>
--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -1,6 +1,38 @@
+
+
 <template>
   <div class="play">
     <h1>This is the play page</h1>
     <p>For playing the instrument</p>
   </div>
+
+  <div id="app">
+    <app-button buttonText="Note 1"/> <br>
+    <app-button buttonText="Note 2"/> <br>
+    <app-button buttonText="Note 3"/>
+  </div>
+
 </template>
+
+<script>
+  import AppButton from "../components/AppButton.vue"
+
+    export default {
+      name: "App",
+      components: {
+         AppButton 
+       },
+      data() {
+          return {
+          }
+      },
+      methods: {
+        sayhello(){
+          alert('hello');
+        },
+      }
+    }
+
+
+</script>
+
