99e60fb7965ecfee1eaaa0c13bff67e904ec42e3
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2023-06-14 16:38:14 +0100

    custom slider for on/off

--- src/components/StringSettings.vue
+++ src/components/StringSettings.vue
@@ -1,57 +1,64 @@
 <template>
-    <div 
-      class="string-settings" 
-      :style="myStyle" 
-    >
-        <span>
-            <input type="checkbox" v-model="checked" checked-name="check" />
-            <label for="check">{{ checked }}</label> 
-        </span>
-    </div>
-  </template>
   
-  
-  <script>
-  
-    export default {
-      props: {
-        buttonColour: String,
-        active: Boolean,
-        number: Number,
-        updateStrings: Function
-      },
-      data() {
-        return {
-          myStyle:{
-          backgroundColor: this.buttonColour,
-          },
-          
-        }
-      },
-      computed: {
-        checked: {
-          get: function() {
-            return this.active;
-          },
-          set: function() {
-            this.updateStrings(this.number, "stringOn", !this.checked)
-          }
-        }
-        
+
+  <div 
+    class="string-settings" 
+    :style="myStyle" 
+  >
+      <span>
+        <SlideToggle class="slide" :value=checked @input=valueLog />
+        <label for="check">{{ onOrOff }}</label> 
+      </span>
+  </div>
+</template>
+
+
+<script>
+
+  import SlideToggle from '@/components/SlideToggle.vue';
+
+  export default {
+    components: {
+      SlideToggle
+    },
+    props: {
+      buttonColour: String,
+      active: Boolean,
+      number: Number,
+      updateStrings: Function
+    },
+    data() {
+      return {
+        myStyle:{
+        backgroundColor: this.buttonColour,
+        },
+        onOrOff: "on"
       }
-    }
-  </script>
-  
-  <style scoped>
-    .string-settings {
-      color: rgb(255, 255, 255);
-      border-radius: 25px;
-      height: 20%;
-      font-size: 4em;
-      display: flex;
-      justify-content: center;
-      flex-direction: column;
-      user-select: none;
-    }
-  </style>
-  
+    },
+    methods: {
+      valueLog(value) {
+        this.updateStrings(this.number, "stringOn", value)
+        this.onOrOff = value ? "on" : "off"
+      }
+    },
+    computed: {
+      checked() {
+          return this.active;
+        }
+      }
+  }
+</script>
+
+<style scoped>
+  .string-settings {
+    color: rgb(255, 255, 255);
+    border-radius: 25px;
+    height: 20%;
+    font-size: 1em;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    user-select: none;
+  }
+
+</style>
